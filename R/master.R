7c3c6c51bf33db4a3ea3ab945ad607b78106f7c1
Mike Ackerman <mackerman@qcinc.org>
2016-09-21 18:56:10 -0600

    Fixed documentation for the master.csv table

---
+++ R/master.R
@@ -0,0 +1,9 @@
+#' @title A Registry of Baseball People
+#'
+#' @description The Master table is a registry of every player and manager who have appeared in the MLB. From Lahman database.
+#' @name master
+#' @docType data
+#' @usage master
+#' @format Master.csv file from the Lahman database
+#' @keywords master baseball lahman
+NULL
