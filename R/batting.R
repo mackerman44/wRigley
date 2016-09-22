89124d9067befcc927a681fb12f220842866c723
Mike Ackerman <mackerman@qcinc.org>
2016-09-21 19:06:40 -0600

    Fixing documentation for Batting and Master data

---
+++ R/batting.R
@@ -0,0 +1,9 @@
+#' @title Individual Batting Statistics
+#'
+#' @description Player batting statistics by season and team from 1871 to 2015
+#' @name batting
+#' @docType data
+#' @usage batting
+#' @format Batting.csv file from the Lahman database
+#' @keywords batting baseball lahman
+NULL
