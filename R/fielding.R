e5f7b6528230bae65de0ca22439b5dd743d7421f
Mike Ackerman <mackerman@qcinc.org>
2016-09-21 20:00:10 -0600

    Added fielding and team data and descriptions

---
+++ R/fielding.R
@@ -0,0 +1,9 @@
+#' @title Player Fielding Data
+#'
+#' @description Season-by-season fielding statistics for players
+#' @name fielding
+#' @docType data
+#' @usage fielding
+#' @format Fielding.csv file from the Lahman database
+#' @keywords fielding baseball lahman
+NULL
---
+++ R/teams.R
@@ -0,0 +1,9 @@
+#' @title Seasonal Team Data
+#'
+#' @description Seasonal data at the team level going back to 1871
+#' @name teams
+#' @docType data
+#' @usage teams
+#' @format Teams.csv file from the Lahman database
+#' @keywords teams baseball lahman
+NULL
