2c55dfd046812d81e51be8cc777ab367c2bbb4a4
Mike Ackerman <mackerman@qcinc.org>
2016-09-21 19:53:01 -0600

    Added pitching data from Lahman database

---
+++ R/pitching.R
@@ -0,0 +1,9 @@
+#' @title Individual Pitching Statistics
+#'
+#' @description Season-by-season pitching data for players
+#' @name pitching
+#' @docType data
+#' @usage pitching
+#' @format Pitching.csv file from the Lahman database
+#' @keywords pitching baseball lahman
+NULL
